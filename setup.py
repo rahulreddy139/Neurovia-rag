61d94de52dce3cad8b8d3f5f097ffeabfbaf0d30
rahulreddy139 <rahulreddy00139@gmail.com>
2026-08-18 17:47:20 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Neurovia-rag",
+    version="0.1.0",
+    author="Rahul reddy",
+    author_email="rahulreddy00139@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
